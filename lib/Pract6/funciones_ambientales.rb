545ebfb796b0bbf83a8053d410065e44c75dd880
Sergio Guerra <alu0101133201@ull.edu.es>
2019-11-14 16:07:39 +0000

    Desarrollo de pruebas corespondientes a la séptima prueba (impacto ambiental mujer) -- Pract6
      Probando los getters y el to_s
    
        Instanciación correcta del objeto alimento
    
        Los getters por cantidad funcionan correctamente
    
        El método to_s funciona correctamente
    
      Probando métodos relativos a valor energético
    
        Obtención de información calórica de un elemento
    
        Comparación para saber si un elemento es más calórico que otro
    
      Probando las funciones relativas al impacto ambiental diario
    
        El calculo del impacto ambiental - hombre funciona correctamente
    
        El calculo del impacto ambiental - mujer funciona correctamente
    
    Finished in 0.00431 seconds (files took 0.13796 seconds to load)
    
    7 examples, 0 failures

--- lib/Pract6/funciones_ambientales.rb
+++ lib/Pract6/funciones_ambientales.rb
@@ -20,9 +20,35 @@ def impacto_ambiental_h(alimentos)
     return [-1,-1]
 
   else 
-    return [gei,terreno]
+    return [gei.round(2),terreno.round(2)]
   end
 
 end
 
 
+
+def impacto_ambiental_m(alimentos)
+
+  proteinas = 0
+  kcal = 0
+  gei = 0
+  terreno = 0
+
+
+  for i in alimentos
+
+    proteinas += i[0].prot(i[1])
+    kcal += i[0].energia(i[1])
+    gei += i[0].g(i[1])
+    terreno += i[0].t(i[1])
+
+
+  end
+
+  if((proteinas < 41) || (kcal < 2300))
+    return [-1,-1]
+
+  else 
+    return [gei.round(2),terreno.round(2)]
+  end
+end 
