f24e4a77253089056f3d4eae5caca72d8be735c5
Sergio Guerra <alu0101133201@ull.edu.es>
2019-11-27 19:12:09 +0000

    Desarrollo del código relativo a la prueba de obtención de terreno usado en la clase plato2 --Práct8
    Pract6
    
      Probando los getters y el to_s
    
        Instanciación correcta del objeto alimento
    
        Los getters por cantidad funcionan correctamente
    
        El método to_s funciona correctamente
    
      Probando métodos relativos a valor energético
    
        Obtención de información calórica de un elemento
    
        Comparación para saber si un elemento es más calórico que otro
    
      Probando las funciones relativas al impacto ambiental diario
    
        El calculo del impacto ambiental - hombre funciona correctamente
    
        El calculo del impacto ambiental - mujer funciona correctamente
    
    Nodo
    
      Pruebas básicas de la clase nodo
    
        Instanciación correcta del objeto nodo
    
    Lista
    
      Pruebas básicas de la clase lista
    
        Instanciación correcta de la lista
    
        Prueba de que la lista está vacía
    
        Prueba de que se puede insertar un elemento en la lista
    
        Prueba de que el to_s funciona
    
      Pruebas de la clase lista
    
        Prueba de que sepueden insertar varios elementos en la lista
    
        Prueba de que se puede extraer el primer elemento de la lista
    
        Prueba de que se puede extraer el último elemento de la lista
    
      Pruebas de las dietas
    
        Prueba de emisiones diarias de gei
    
        Prueba de emisiones anules de gei
    
        Prueba de terreno usado a diario
    
        Prueba de terreno usado anual
    
    Alimento
    
      Pruebas de módulos
    
        Prueba de que los alimentos son comparables (a nivel energético)
    
        Prueba de que la lista doblemente enlazada sea enumerable
    
    Plato
    
      Pruebas relativas a la clase plato
    
        Prueba de que los obj. plato se instancian correctamente (obtención de atributos de manera correcta)
    
        Prueba de los métodos que obtienen el tanto por ciento de los valores nutricionales de un plato
    
        Prueba de obtención del valor calórico total de un plato
    
        Prueba del fromateo del plato (to_s)
    
    Plato2
    
      Pruebas básicas de la clase plato2
    
        Prueba de obtención de las emisiones diarias de gei
    
        Prueba de obtención de terreno usado
    
    Finished in 0.01019 seconds (files took 0.11307 seconds to load)
    
    27 examples, 0 failures

--- lib/Pract6/plato2.rb
+++ lib/Pract6/plato2.rb
@@ -24,4 +24,23 @@ class Plato2 < Plato
   end
 
 
+  def terreno
+
+    gramos_totales = cantidades.sum
+
+    x = alimentos.head
+    y = cantidades.head
+    total = 0
+
+    while( x != nil)
+
+      porcentaje_individual = (y.value*100)/gramos_totales
+      total += (x.value.terreno * porcentaje_individual)/100
+      x = x.next
+
+    end
+    return (total.round(2))
+
+  end
+
 end
